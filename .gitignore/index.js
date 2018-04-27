0be03cc4ec56feed371527436cb4b01ad8a3eb7f
GML-royaleTV <38780788+GML-royaleTV@users.noreply.github.com>
2018-04-27 16:41:57 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -8,7 +8,7 @@ bot.on('ready', function() {
     console.log("Connectedç")
 });
 
-bot.login("NDM4MzA0MTY0MDg3MzMyODY2.DcDH4A.O6HqRLNa5tk48hHD1AOCLx52LqI");
+bot.login(process.env.TOKEN);
 
 
 bot.on('message', message => {
